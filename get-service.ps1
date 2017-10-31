af3b3d62fbe949f080c0064618740d83bef4643b
sajiduk159 <31134759+sajiduk159@users.noreply.github.com>
2017-10-31 12:04:46 +0000

    Update get-service.ps1

--- get-service.ps1
+++ get-service.ps1
@@ -1,4 +1,5 @@
 #Script to check a list of servers for any given service and report back whether it is represent 
+#Todo: use Invoke-Command -Session for synchronous run of command on all servers
 
 $servers = "C:\Temp\servers.txt"
 $service = "My Service Display Name"
